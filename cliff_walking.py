d08e1e4ab66b82becbc73bb42ccbe7a825516349
Jaewook Choi <eari100@naver.com>
2023-11-12 00:17:11 +0900

    초기 정책 추가

--- cliff_walking.py
+++ cliff_walking.py
@@ -1,12 +1,22 @@
 import gymnasium as gym
+import numpy as np
 
 
 env = gym.make('CliffWalking-v0', render_mode="human")
 observation, info = env.reset()
 
-for _ in range(30):
+# 0: 위, 1: 오른쪽, 2: 아래, 3: 왼쪽으로 이동
+theta_0 = np.array([
+    [np.nan, 1, 1, np.nan], [np.nan, 1, 1, 1], [np.nan, 1, 1, 1], [np.nan, 1, 1, 1], [np.nan, 1, 1, 1], [np.nan, 1, 1, 1], [np.nan, 1, 1, 1], [np.nan, 1, 1, 1], [np.nan, 1, 1, 1], [np.nan, 1, 1, 1], [np.nan, 1, 1, 1], [np.nan, np.nan, 1, 1],
+    [1, 1, 1, np.nan], [1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1], [1, np.nan, 1, 1],
+    [1, 1, 1, np.nan], [1, 1, np.nan, 1], [1, 1, np.nan, 1], [1, 1, np.nan, 1], [1, 1, np.nan, 1], [1, 1, np.nan, 1], [1, 1, np.nan, 1], [1, 1, np.nan, 1], [1, 1, np.nan, 1], [1, 1, np.nan, 1], [1, 1, np.nan, 1], [1, np.nan, 1, 1],
+    [1, np.nan, np.nan, np.nan]
+])
+
+for _ in range(0):
     action = env.action_space.sample()  # 행동 랜덤
-    observation, reward, terminated, truncated, info = env.step(action) # 역학 1단계 실행
+    # observation: 위치
+    observation, reward, terminated, truncated, info = env.step(0) # 역학 1단계 실행
 
     if terminated or truncated:
         observation, info = env.reset()
